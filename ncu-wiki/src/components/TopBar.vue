f2cc6d55bbbef693db1a734c41d9187b40d5950c
RedNeath <theandre.bourry@gmail.com>
2022-09-08 22:58:29 +0200

    TopBar designed, all scripts yet to make

--- ncu-wiki/src/components/TopBar.vue
+++ ncu-wiki/src/components/TopBar.vue
@@ -1,11 +1,45 @@
 <template>
-    <v-btn
-        elevation="2"
-        icon
-        large
-        outlined
-        tile
+    <v-container id="topBar"
+        fluid
+        class="pa-0"
     >
-        <v-icon>mdi-heart</v-icon>
-    </v-btn>
+        <v-row align="center">
+            <v-col
+                cols="12"
+                sm="6"
+            >
+                <div id="title" class="text-left">
+                    <h1>
+                        NcuWiki
+                    </h1>
+                </div>
+            </v-col>
+
+            <v-col
+                cols="12"
+                sm="6"
+            >
+                <div id="buttons" class="text-right">
+                    <v-btn
+                        elevation="2"
+                        icon
+                        large
+                        outlined
+                        tile
+                    >
+                        <v-icon>mdi-magnify</v-icon>
+                    </v-btn>
+                    <v-btn
+                        elevation="2"
+                        icon
+                        large
+                        outlined
+                        tile
+                    >
+                        <v-icon>mdi-theme-light-dark</v-icon>
+                    </v-btn>
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
