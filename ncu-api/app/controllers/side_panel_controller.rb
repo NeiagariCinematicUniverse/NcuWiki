b8fdd35fbc049a421cd776a8cb4ea50627d8ce87
RedNeath <theandre.bourry@gmail.com>
2023-01-02 12:45:13 +0100

    [FIX] Separation of the API and the Web-App in 2 repositories

--- ncu-api/app/controllers/side_panel_controller.rb
+++
@@ -1,6 +0,0 @@
-class SidePanelController < ApplicationController
-    def side
-        dataStream = File.open("./side_panels/" + params[:id] + ".md")
-        @pageContent = dataStream.read()
-    end
-end
