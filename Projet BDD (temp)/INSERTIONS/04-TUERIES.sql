6337eaa06fd57e389e0fd2d4e45d1c9a76dc8076
Héloïse Lalande <heloise.lalande@etu.univ-nantes.fr>
2023-04-12 17:33:50 +0200

    [WIP] complétion du script d'insertion des langues et création/complétion du script d'insertion des Tueries

---
+++ Projet BDD (temp)/INSERTIONS/04-TUERIES.sql
@@ -0,0 +1,29 @@
+USE NCU_BDD;
+
+DELETE FROM Tueries;
+
+INSERT INTO Tueries (titre, code, annee) VALUES
+    ("L'Enfer Aquatique", "EA", 2016),
+    ("Despair Vacation", "DV", 2016),
+    ("Heart of Despair", "HOD", 2016),
+    ("Le Bal des Pantins", "LBDP", 2017),
+    ("Un Blanc Voile Sépulcral", "UBVS", 2017),
+    ("Abyss of Despair", "AOD", 2018),
+    ("Filum Praeterita", "FP", 2018),
+    ("Le Songe du Papillon", "LSDP", 2019),
+    ("The Art of Creating Hope", "TAOCH", 2019),
+    ("The Last Dinner Party", "TLDP", 2019),
+    ("Hôtel des Espérés", "HDE", 2020),
+    ("Ruée vers l'Espoir", "RVE", 2020),
+    ("Mysteries will be solved", "MWBS", 2021),
+    ("Requiem pour nos Coeurs plein d'Espoirs", "RPNCPE", 2021),
+    ("Collapsing Justice", "EA", 2022),
+    ("Babel's Curse", "BC", 2022),
+    ("A l'ombre de nos morts", "AODNM", 2023),
+    ("Observing End", "OE", 2023),
+    ("Le Son du Silence", "LSS", 2024),
+    ("Ode to Sorrow", "OS", 2024),
+    ("Regis Legatum", "RL", 2025),
+    ("Perros de Guerra", "PDG", 2025),
+    ("Sacrement Sépulcral", "SS", 2026)
+;
